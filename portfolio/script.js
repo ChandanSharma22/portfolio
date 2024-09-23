b61fed56e09aa7b91ff2849bb39c55df7a88f74a
Chandan Sharma <chandansharma9008@gmail.com>
2024-09-23 16:33:31 +0530

    new

---
+++ portfolio/script.js
@@ -0,0 +1,21 @@
+console.log("Script running...");
+
+
+/*document.querySelector('.close').style.display = 'none';
+document.querySelector('.hamburger').addEventListener("click", () => {
+    const sidebar = document.querySelector('.sidebar');
+    const infoContainer = document.querySelector('.infoContainer');
+    sidebar.classList.toggle('sidebarGo');
+    
+    if(sidebar.classList.contains('sidebarGo')) {
+        document.querySelector('.menu').style.display = 'inline';
+        document.querySelector('.close').style.display = 'none';
+       
+    } else {
+        setTimeout(() => {
+            document.querySelector('.close').style.display = 'inline';
+        }, 300); // Increased delay to allow sidebar animation to complete
+        document.querySelector('.menu').style.display = 'none';
+       
+    }
+});*/
